d5dea57dfd6f6fd695c1a7a6b07f3f167cc35274
philbort <phduan@gmail.com>
2016-04-26 18:44:01 -0700

    171 Excell sheet column number

---
+++ 171_excel_sheet_column_number.cpp
@@ -0,0 +1,32 @@
+/*
+
+171. Excel Sheet Column Number
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+
+*/
+
+class Solution {
+
+public:
+
+    int titleToNumber(string s) {
+
+        int res = 0;
+
+        for (int i = 0; i < s.size(); i++)
+            res = s[i] - 'A' + 1 + res * 26;
+        
+        return res;
+    }
+};

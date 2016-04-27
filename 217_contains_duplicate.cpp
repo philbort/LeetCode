29926b33500533ea856bc03415a26d0676a9b462
philbort <phduan@gmail.com>
2016-04-26 18:52:20 -0700

    217 Contains duplicate

---
+++ 217_contains_duplicate.cpp
@@ -0,0 +1,21 @@
+/*
+
+217. Contains Duplicate
+
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+*/
+
+#include <set>
+using namespace std;
+
+class Solution {
+
+public:
+
+    bool containsDuplicate(vector<int>& nums) {
+    	
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+    }
+};

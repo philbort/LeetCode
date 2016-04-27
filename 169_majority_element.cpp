22299a7f22b093920adaa7def29c15cd2605c1c8
philbort <phduan@gmail.com>
2016-04-26 18:59:53 -0700

    169 Majority element

---
+++ 169_majority_element.cpp
@@ -0,0 +1,44 @@
+/*
+
+169. Majority Element 
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+*/
+
+class Solution {
+
+public:
+
+    int majorityElement(vector<int>& nums) {
+
+        std::sort(nums.begin(),nums.end());
+
+        return nums[nums.size()/2];
+    }
+};
+
+/*
+
+O(n) solution:
+
+public class Solution {
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+
+        }
+        return major;
+    }
+}
+
+*/

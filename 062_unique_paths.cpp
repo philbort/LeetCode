78270b7b838063b1ede233b0c172eee4532192d2
philbort <phduan@gmail.com>
2016-04-28 18:37:58 -0700

    62 Unique paths

---
+++ 062_unique_paths.cpp
@@ -0,0 +1,36 @@
+/*
+
+62. Unique Paths
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Note: m and n will be at most 100.
+
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        // Use double to avoid over flow
+        double ans = 1;
+        
+        // total choosen combo
+        int total = m - 1 + n - 1;
+        int combo = n-1 > m-1? m-1 : n-1;
+        if (total == combo)
+            return 1;
+            
+        for (int i = 0; i < combo; i++)
+            ans *= (total-i);
+        for (int i = combo; i > 1; i--)
+            ans /= i;
+        return (int)ans;
+        
+    }
+};

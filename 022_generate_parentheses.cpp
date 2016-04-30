1c58c5a534b4e338fa782ddbb7af7420a9550227
philbort <phduan@gmail.com>
2016-04-30 14:14:13 -0700

    22 Generate parenthese

---
+++ 022_generate_parentheses.cpp
@@ -0,0 +1,31 @@
+/*
+
+22. Generate Parentheses 
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
+
+*/
+
+
+// WTF
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        helper(ret, "", n, n);
+        return ret;
+    }
+    void helper(vector<string> & res, string str, int left, int right){
+        if(left == 0 && right == 0){
+            res.push_back(str);
+            return;
+        }
+        if(left > 0) helper(res, str + "(", left - 1, right);
+        if(right > left) helper(res, str + ")", left, right - 1);
+    }
+};

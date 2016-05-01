a9c9c9882514f417459ccd7978c06713c523c49d
philbort <phduan@gmail.com>
2016-05-01 11:00:47 -0700

    326 Power of three

---
+++ 326_power_of_three.cpp
@@ -0,0 +1,32 @@
+/*
+
+326. Power of Three
+
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+
+*/
+
+class Solution {
+
+public:
+	
+    bool isPowerOfThree(int n) {
+        
+        double ans = log10(n)/log10(3);
+        
+        return ans == (int) ans;
+        
+    }
+};
+
+/*
+
+bool isPowerOfThree(int n) {
+    int maxPowerOfThree = (int)Math.pow(3, (int)(Math.log(0x7fffffff) / Math.log(3)));
+    return n>0 && maxPowerOfThree%n==0;
+}
+
+*/

c32d0c965056a6a36d39b09e61b702c64d9590d5
philbort <phduan@gmail.com>
2016-05-07 11:03:36 -0700

    add iterative solution

--- 024_swap_nodes_in_pairs.cpp
+++ 024_swap_nodes_in_pairs.cpp
@@ -34,3 +34,29 @@ public:
         return next;
     }
 };
+
+
+/* iteratively
+
+    ListNode *swapPairs(ListNode *head) {
+
+        ListNode *p = new ListNode(0);
+        p->next = head;
+        head = p;
+
+        while(true){
+
+            if (!p->next || !p->next->next)
+            	break; 
+
+            ListNode* q1 = p->next;
+            ListNode* q2 = q1->next;
+            q1->next = q2->next;
+            q2->next = q1;
+            p->next = q2;
+            p=q1;
+        } 
+
+        return head->next;
+    }
+};

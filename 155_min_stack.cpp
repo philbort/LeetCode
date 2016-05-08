0f66a2b1fd79578bd43657e8dfc0d3c082b0f8a9
philbort <phduan@gmail.com>
2016-05-08 14:51:14 -0700

    155 Min stack

---
+++ 155_min_stack.cpp
@@ -0,0 +1,95 @@
+/*
+
+155. Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+
+*/
+
+class MinStack {
+
+public:
+    void push(int x) {
+        s.push(x);
+        if(min_s.empty() || x <= min_s.top())
+            min_s.push(x);
+    }
+    
+    void pop() {
+        if( s.top() == min_s.top())
+            min_s.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min_s.top();
+    }
+    
+private:
+    stack<int> s, min_s;
+};
+
+/* with only one stack:
+   however,  
+   the possible gap between the current value and the min value could be Integer.MAXVALUE-Integer.MINVALUE;
+
+public class MinStack {
+    long min;
+    Stack<Long> stack;
+
+    public MinStack(){
+        stack=new Stack<>();
+    }
+
+    public void push(int x) {
+        if (stack.isEmpty()){
+            stack.push(0L);
+            min=x;
+        }else{
+            stack.push(x-min);//Could be negative if min value needs to change
+            if (x<min) min=x;
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+
+        long pop=stack.pop();
+
+        if (pop<0)  min=min-pop;//If negative, increase the min value
+
+    }
+
+    public int top() {
+        long top=stack.peek();
+        if (top>0){
+            return (int)(top+min);
+        }else{
+           return (int)(min);
+        }
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+}
+
+*/

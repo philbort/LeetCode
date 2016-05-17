4a13dd65ef466d78687ab2f1f5d7e86586fd5d49
philbort <phduan@gmail.com>
2016-05-16 17:46:46 -0700

    303 Range sum query

---
+++ 303_range_sum_query.cpp
@@ -0,0 +1,48 @@
+/*
+
+303. Range Sum Query - Immutable
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+
+*/
+
+class NumArray {
+
+public:
+
+    NumArray(vector<int> &nums) {
+        int n = nums.size();
+        if (n < 1)
+            return;
+        sum.resize(n);
+        sum[0] = nums[0];
+        for (int i = 1; i < n; i++){
+            sum[i] = sum[i-1] + nums[i];
+        }
+    }
+
+    int sumRange(int i, int j) {
+        return i? ( sum[j] - sum[i-1] ) : sum[j];
+    }
+
+private:
+	
+    vector<int> sum;
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);

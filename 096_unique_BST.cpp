466ee6071c5725367760a6fa8aa59007c7381305
philbort <phduan@gmail.com>
2016-06-11 12:18:57 -0700

    add comments

--- 096_unique_BST.cpp
+++ 096_unique_BST.cpp
@@ -18,28 +18,35 @@ Given n = 3, there are a total of 5 unique BST's.
 class Solution {
 
 public:
-
+    
     int numTrees(int n) {
-
-        if(n < 2)
-            return 1;
         
-        // Initialize double to avoid overflow
-        vector<double> cnt(n+1);
-        
-        cnt[0] = 1;
-        cnt[1] = 1;
+        // 1 for 0 or 1 node
+        if(n < 2)   return 1;
+        vector<int> cnt(n+1);
+        cnt[0] = cnt[1] = 1;
         
         for (int i = 2; i <= n; i++)
         {
+           /* Cycle through the root,
+           i.e., how many on the left side of 
+           the root and how many on the right
+           side of the root.
+           Only do half (i/2) since the other
+           half is the same
+           */
             for(int j = 0; j < i/2; j++)
             {
                 cnt[i] +=cnt[j] * cnt[i-j-1];
             }
+            // Multiply for the other half
             cnt[i] *= 2;
+            /* if even number of node, include 
+            the middle one (non-symmetric one)
+            */
             if(i%2)
                 cnt[i] += cnt[i/2]*cnt[i/2];
         }
-        return (int) cnt[n];
+        return cnt[n];
     }
 };

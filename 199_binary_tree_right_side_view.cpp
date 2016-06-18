75a8ece027c991a4b97e59bed7b2aaadea616b4f
philbort <phduan@gmail.com>
2016-06-18 14:49:10 -0700

    199 Binary tree right side view

---
+++ 199_binary_tree_right_side_view.cpp
@@ -0,0 +1,67 @@
+/*
+
+199. Binary Tree Right Side View
+
+Given a binary tree, imagine yourself standing on the right side of it, 
+return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	// Level traversal, only save the last node per level
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if(!root)   return result;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty()) {
+            result.push_back(q.back()->val);
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+        }
+        return result;
+    }
+};
+
+/* Very smart recursion solution
+
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()<level) res.push_back(root->val);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+    }
+
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> res;
+        recursion(root, 1, res);
+        return res;
+    }
+    
+*/

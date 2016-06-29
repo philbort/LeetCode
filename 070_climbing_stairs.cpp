011e28a8f651e6b919ae3b5bb4be03b8740e1da7
philbort <phduan@gmail.com>
2016-06-28 18:45:34 -0700

    O(1) space complexity

--- 070_climbing_stairs.cpp
+++ 070_climbing_stairs.cpp
@@ -11,15 +11,28 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 class Solution {
 
 public:
+    // O(1) space
+    int climbStairs(int n) {
+        if ( n < 2 )
+            return 1;
+        vector<int> cnt(2,1);
+        for (int i = 2; i <= n; i++)
+            cnt[i%2] = cnt[0] + cnt[1];
+        return cnt[n%2];
+    }
+};
+
+
+/* O(n) space
 
     int climbStairs(int n) {
-
         vector<int> cnt(n+1,0);
-        
         cnt[0] = cnt[1] = 1;
         
         for (int i = 2; i <= n; i++)
             cnt[i] = cnt[i-1] + cnt[i-2];
         return cnt[n];
     }
-};
+    
+*/
+    

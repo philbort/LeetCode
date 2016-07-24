4ad9590458ca6b2fbd763af1593a59f8e274c386
philbort <phduan@gmail.com>
2016-07-23 17:02:45 -0700

    Add iterative solution

--- 090_subsets_II.cpp
+++ 090_subsets_II.cpp
@@ -23,6 +23,37 @@ If nums = [1,2,2], a solution is:
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        /* For alll the duplicates, use "cnt" to count how 
+           many times they appear. In the result, the duplicate
+           should either appear none, once, twice, .. till cnt.
+        */
+        vector<vector<int>> result(1, vector<int>());
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size()) {
+            // Track the number of duplicate apperance
+            int cnt = 0;
+            while(i + cnt < nums.size() && nums[i+cnt] == nums[i])
+                cnt++;
+            int m = result.size();
+            for (int j = 0; j < m; j++) {
+                vector<int> current = result[j];
+                // All the combination for the duplicates
+                for(int k = 0; k < cnt; k++) {
+                    current.push_back(nums[i]);
+                    result.push_back(current);
+                }
+            }
+            i += cnt;
+            
+        }
+        return result;
+    }
+};
+
+/* Recursion:
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
         vector<int> current;
@@ -39,6 +70,6 @@ private:
                 current.pop_back();
             }
         }
-    }    
+    }   
     
-};
+*/

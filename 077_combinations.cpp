8df8d955b0390dcb54cef88d1909394a42dbea3e
philbort <phduan@gmail.com>
2016-07-30 14:45:01 -0700

    77 Combinations

---
+++ 077_combinations.cpp
@@ -0,0 +1,43 @@
+/*
+
+77. Combinations
+
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> cur;
+        helper(1, n, k, cur, result);
+        return result;
+    }
+private:
+    void helper(int start, int end, int k, vector<int>& cur, vector<vector<int>>& result) {
+        if (!k) {
+            result.push_back(cur);
+            return;
+        }
+        for(int i = start; i <= end; i++) {
+            cur.push_back(i);
+            helper(i+1, end, k-1, cur, result);
+            cur.pop_back();
+        }
+        
+    }
+};
+

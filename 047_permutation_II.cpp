54b0efc7a91fa4c10594430be1f843878bde76f9
philbort <phduan@gmail.com>
2016-08-03 20:13:47 -0700

    add pass by reference solution

--- 047_permutation_II.cpp
+++ 047_permutation_II.cpp
@@ -24,17 +24,56 @@ public:
     }
     
 private:
+    /* The swapping method is based on the array is sorted. 
+       If passing by reference, after swapping the array is NOT sorted anymore.
+       So we have to do additional bookkeeping to make sure we are not swapping
+       the duplicates.
+   */
+	void permutation( vector<int>& nums, int ind, vector<vector<int>>& result) {
+		if (ind >= nums.size() - 1) {
+			result.push_back(nums);
+			return;
+		}
+		for (int i = ind; i < nums.size(); i++) {
+		    // We do the regular check
+		    if ( i != ind && nums[ind] == nums[i])
+		        continue;
+		    // Then we make sure the current number
+		    // hasn't been swapped before.
+            bool duplicate = false;
+	        if(i != ind){
+	            for(int j = ind; j < i; j++) {
+	                if(nums[j] == nums[i]) {
+	                    duplicate = true;
+	                    break;
+	                }
+	            }
+	        }
+	        if(!duplicate) {
+		        swap( nums[ind],nums[i]);
+    			permutation(nums, ind+1, result);
+    			swap(nums[i], nums[ind]);
+	        }
+		}
+	}
+};
+
+/*
+     pass by value here:
+       The swapping method is based on the array is sorted. 
+       If passing by value, bookkeeping is much easier, we don't need to 
+       swap back.
+    
 	void permutation( vector<int> nums, int ind, vector<vector<int>>& result) {
 		if (ind >= nums.size() - 1) {
 			result.push_back(nums);
 			return;
 		}
 		for (int i = ind; i < nums.size(); i++) {
-		    if ( i != ind && nums[ind] == nums[i])// || nums[i] == nums[i-1]) )
+		    if ( i != ind && nums[ind] == nums[i])
 		        continue;
 		    std::swap( nums[ind],nums[i]);
 			permutation(nums, ind+1, result);
-			//std::swap(nums[i], nums[ind]);
 		}
 	}
-};
+*/

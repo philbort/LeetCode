e6c31dccb052da1f00ee8e9e465c9a70cdc0f7db
philbort <phduan@gmail.com>
2016-08-17 20:09:57 -0700

    add the fancy solution from the forum

--- 051_n_queens.cpp
+++ 051_n_queens.cpp
@@ -68,3 +68,51 @@ private:
     }
     int n;
 };
+
+/* fancy solution to track the diagonals:
+
+The number of columns is n, the number of 45° diagonals is 2 * n - 1, the number of 135° diagonals is also 2 * n - 1. 
+When reach [row, col], the column No. is col, the 45° diagonal No. is row + col and the 135° diagonal No. is n - 1 + col - row. 
+We can use three arrays to indicate if the column or the diagonal had a queen before, if not, we can put a queen in this position and continue.
+
+  *    | | |                / / /             \ \ \
+  *    O O O               O O O               O O O
+  *    | | |              / / / /             \ \ \ \
+  *    O O O               O O O               O O O
+  *    | | |              / / / /             \ \ \ \ 
+  *    O O O               O O O               O O O
+  *    | | |              / / /                 \ \ \
+  *   3 columns        5 45° diagonals     5 135° diagonals    (when n is 3)
+  
+class Solution {
+public:
+    std::vector<std::vector<std::string> > solveNQueens(int n) {
+        std::vector<std::vector<std::string> > res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        std::vector<int> flag_col(n, 1), flag_45(2 * n - 1, 1), flag_135(2 * n - 1, 1);
+        solveNQueens(res, nQueens, flag_col, flag_45, flag_135, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(std::vector<std::vector<std::string> > &res, 
+                      std::vector<std::string> &nQueens, 
+                      std::vector<int> &flag_col, 
+                      std::vector<int> &flag_45, 
+                      std::vector<int> &flag_135, 
+                      int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (flag_col[col] && flag_45[row + col] && flag_135[n - 1 + col - row]) {
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 0;
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, flag_col, flag_45, flag_135, row + 1, n);
+                nQueens[row][col] = '.';
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 1;
+            }
+    }
+};
+
+*/

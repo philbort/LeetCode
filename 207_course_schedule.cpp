ffdd29e44e6616823fde287e759453d71189cfc8
philbort <phduan@gmail.com>
2016-08-24 19:38:37 -0700

    better bfs solution

--- 207_course_schedule.cpp
+++ 207_course_schedule.cpp
@@ -20,26 +20,88 @@ To take course 1 you should have finished course 0, and to take course 0 you sho
 
 */
 
+class Solution {
+public:
+    //  For BFS, we map the indegree of all the nodes, and we find the
+    //  nodes without any prerequisites (i.e., zeroInDegree). We start with 
+    //  zeroInDegree and we traverse all the nodes to eliminate their indegrees
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        
+        // If no course to take or no prerequisites, of course we can finish
+        if(!numCourses || prerequisites.empty())  
+            return true;
+            
+        // Hash table with known size
+        vector<unordered_set<int>> graph(numCourses);   
+        
+        // Queue for zero indegrees
+        queue<int> zeroInDegree;
+        // Number of indegrees
+        vector<int> InDegree(numCourses, 0);
+        
+        // Transform edge lists to adjacency lists
+        for (int i = 0; i < prerequisites.size(); i++) {
+            graph[prerequisites[i].second].insert(prerequisites[i].first);
+        }
+        
+        // Check number of indegrees
+        for (int i = 0; i < numCourses; i++) {
+            for(int node: graph[i])
+                InDegree[node]++;
+        }
+        
+        // Queue the zero indegree courses
+        for (int i = 0; i < numCourses; i++) {
+            if(InDegree[i] == 0)
+                zeroInDegree.push(i);
+        }
+        
+        while(!zeroInDegree.empty()) {
+            // The current course
+            int course = zeroInDegree.front();
+            zeroInDegree.pop();
+            // Find all the courses require the current course
+            for (int node: graph[course]) {
+                // indegree minus 1 for these courses
+                InDegree[node]--;
+                // If a course doesn't have any indegrees, put it in the queue
+                if(InDegree[node] == 0)
+                    zeroInDegree.push(node);
+            }
+        }
+        
+        // If we still have indegrees for any course, we hace a cycle
+        for (int i = 0; i < numCourses; i++) {
+            if(InDegree[i] > 0)
+                return false;
+        }
+            
+        // Othewsise we don't
+        return true;
+    }
+};
 
+
+/*
 
 class Solution {
 public:
-    /*  For DFS, it will first visit a node, then one neighbor of it, 
-        then one neighbor of this neighbor... and so on. If it meets 
-        a node which was visited in the current process of DFS visit, 
-        a cycle is detected and we will return false. Otherwise it will 
-        start from another unvisited node and repeat this process till 
-        all the nodes have been visited. Note that you should make two 
-        records: one is to record all the visited nodes and the other 
-        is to record the visited nodes in the current DFS visit.
-    */
+     // For DFS, it will first visit a node, then one neighbor of it, 
+     // then one neighbor of this neighbor... and so on. If it meets 
+     // a node which was visited in the current process of DFS visit, 
+     // a cycle is detected and we will return false. Otherwise it will 
+     // start from another unvisited node and repeat this process till 
+     // all the nodes have been visited. Note that you should make two 
+     // records: one is to record all the visited nodes and the other 
+     // is to record the visited nodes in the current DFS visit.
+    
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         
         // If no course to take or no prerequisites, of course we can finish
         if(!numCourses || prerequisites.empty())  
             return true;
         
-        // multiset is faster than unordered_set here, why?
+        // Hash table with known size
         vector<unordered_set<int>> graph(numCourses);
         
         // All the visited nodes
@@ -86,3 +148,5 @@ private:
     }
     
 };
+
+*/

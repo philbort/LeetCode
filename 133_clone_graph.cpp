49861f8434aa092748bc38ce9b40802d57669884
philbort <phduan@gmail.com>
2016-08-26 18:17:57 -0700

    initial commit

---
+++ 133_clone_graph.cpp
@@ -0,0 +1,85 @@
+/*
+
+133. Clone Graph
+
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+
+*/
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(!node)    return NULL;
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> map;
+        queue<UndirectedGraphNode*> unvisited;
+        map[node] = new UndirectedGraphNode(node->label);
+        unvisited.push(node);
+        while(!unvisited.empty()) {
+            UndirectedGraphNode* current = unvisited.front();
+            unvisited.pop();
+            for(int i = 0; i < current->neighbors.size(); i++) {
+                if(map.find(current->neighbors[i]) == map.end()) {
+                    map[current->neighbors[i]] = new UndirectedGraphNode(current->neighbors[i]->label);
+                    unvisited.push(current->neighbors[i]);
+                }
+                map[current]->neighbors.push_back(map[current->neighbors[i]]);
+            }
+        }
+        return map[node];
+    }
+};
+
+
+
+/* DFS solution:
+
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        // Base condition
+        if(!node)    return NULL;
+        // If the node is not already cloned
+        if(map.find(node) == map.end()) {
+            // Clone this node
+            map[node] = new UndirectedGraphNode(node->label);
+            // Call recursion on all its neighbors
+            for(int i = 0; i < node->neighbors.size(); i++) {
+                map[node]->neighbors.push_back(cloneGraph(node->neighbors[i]));
+            }
+        }
+        return map[node];
+    }
+private:
+    // Hash table to track the already cloned nodes
+    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> map;
+    
+*/

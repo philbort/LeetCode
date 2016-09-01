dffc49542b151dfdeb3d9c4f98a4f6c5ac8b5914
philbort <phduan@gmail.com>
2016-08-31 18:35:39 -0700

    add another solution

--- 217_contains_duplicate.cpp
+++ 217_contains_duplicate.cpp
@@ -3,19 +3,31 @@
 217. Contains Duplicate
 
 Given an array of integers, find if the array contains any duplicates. 
-Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
 
 */
 
-#include <set>
-using namespace std;
-
 class Solution {
-
 public:
-
+    // Use unordered_map, O(n) time and space
+    // however, O(1) time at best if the duplicate
+    // appears early i.e., no need to traverse the
+    // entire thing
     bool containsDuplicate(vector<int>& nums) {
-    	
-        return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+        unordered_map<int, int> map;
+        for(int num: nums) {
+            if(++map[num] > 1)
+                return true;
+        }
+        return false;
     }
 };
+
+/* Use unordered_set, O(n) time and space
+
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();        
+    }
+    
+*/

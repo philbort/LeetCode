95a92838780d475aadbe25f793bfd33da53671ea
philbort <phduan@gmail.com>
2016-09-02 17:50:56 -0700

    clean up

--- 054_spiral_matrix.cpp
+++ 054_spiral_matrix.cpp
@@ -17,14 +17,14 @@ You should return [1,2,3,6,9,8,7,4,5].
 */
 
 class Solution {
-
 public:
-	
+    // Time: O(m*n)
+    // Space: O(1)
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         
         if (matrix.empty()) return vector<int> ();
         int m = matrix.size(), n = matrix[0].size();
-        // Pre-allocate the known size is faster than push_back
+        // We already know the size
         vector<int> result(m*n);
         
         int left = 0, right = n - 1, up = 0, down = m - 1;
@@ -34,34 +34,25 @@ public:
             // from left to right
             if (left > right)   break;
             for (int i = left; i <= right; i++)
-            {
                 result[ind++] = matrix[up][i];
-            }
             up++;
             
             // from up to down
             if (up > down)  break;
             for (int i = up; i <= down; i++)
-            {
                 result[ind++] = matrix[i][right];
-
-            }
             right--;
             
             // from right to left
             if (left > right)   break;
             for (int i = right; i >= left; i--)
-            {
                 result[ind++] = matrix[down][i];
-            }
             down--;
             
             // from down to up
             if (up > down)  break;
             for (int i = down; i >= up; i--)
-            {
                 result[ind++] = matrix[i][left];
-            }
             left++;
 
         }

aea6b20bccf27e7904dd74e93ee702d6ee75668a
philbort <phduan@gmail.com>
2016-09-04 14:50:12 -0700

    add complexity

--- 002_add_two_numbers.cpp
+++ 002_add_two_numbers.cpp
@@ -19,17 +19,14 @@ Output: 7 -> 0 -> 8
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
+ 
 class Solution {
-
 public:
-	
+    // Time: O(n)
+    // Space: O(1)
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        if( !l1 )
-            return l2;
-        if( !l2 )
-            return l1;
+        if( !l1 )   return l2;
+        if( !l2 )   return l1;
             
         ListNode *head = new ListNode(0);
         ListNode *tmp = head;
@@ -37,13 +34,14 @@ public:
         
         while( l1 || l2 || carry)
         {
-            int sum = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
             carry = sum / 10;
             tmp->next = new ListNode(sum%10);
             tmp = tmp->next;
-            l1 = l1? l1->next:l1;
-            l2 = l2? l2->next:l2;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
         }
         return head->next;
     }
 };
+

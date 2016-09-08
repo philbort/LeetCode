e9f94889b6069f72609c4473f3efda9b1c41c026
philbort <phduan@gmail.com>
2016-09-08 11:27:34 -0700

    clean up

--- 121_buy_sell_stock_I.cpp
+++ 121_buy_sell_stock_I.cpp
@@ -4,16 +4,42 @@
 
 Say you have an array for which the ith element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
- design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
 
 */
 
-
- class Solution {
-
+class Solution {
 public:
+    // Time: O(n)
+    // Space: O(1)
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxPro = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            
+            // The order of the following two commands doesn't matter
+            minPrice = min(minPrice, prices[i]);
+            maxPro = max(maxPro, prices[i] - minPrice);
+        }
+        return maxPro;
+    }
+};
 
+/*
+    // Time: O(n)
+    // Space: O(1)
     // Same as the maximum sub-array question
     int maxProfit(vector<int>& prices) {
         
@@ -30,18 +56,4 @@ public:
         }
         return final_max;
     }
-};
-
-/*
-Even better solution:
-
-int maxProfit(vector<int> &prices) {
-    int maxPro = 0;
-    int minPrice = INT_MAX;
-    for(int i = 0; i < prices.size(); i++){
-        minPrice = min(minPrice, prices[i]);
-        maxPro = max(maxPro, prices[i] - minPrice);
-    }
-    return maxPro;
-}
 */

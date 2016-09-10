1c352f5113acfa192ba42ef6d7e8c2382095143b
philbort <phduan@gmail.com>
2016-09-10 10:31:51 -0700

    add comments

--- 123_best_time_to_buy_and_sell_stock_III.cpp
+++ 123_best_time_to_buy_and_sell_stock_III.cpp
@@ -18,13 +18,23 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         if(n < 2)   return 0;
+        // Buy has to start as INT_MIN, otherwise we never buy
         int buy1 = INT_MIN, sell1 = 0, buy2 = INT_MIN, sell2 = 0;
         for (int i = 0; i < n; i++) {
+            // The below solution uses two states (cur and next)
+            // technically makes more sense.
+            // Here we check sell1 right after buy1, but we aren't
+            // supposed to buy and sell in the same day.
+            // However, if you buy and sell at the same day, you 
+            // basically do nothing and it shouldn't impact the 
+            // result.
             buy1 = max(buy1, -prices[i]);
             sell1 = max(sell1, buy1 + prices[i]);
             buy2 = max(buy2, sell1 - prices[i]);
             sell2 = max(sell2, buy2 + prices[i]);
         }
+        // Only return sell2 (not sell1) because sell2 is up to 2 sells
+        // and it already included the first sell from dynamic programming
         return sell2;
     }
 };

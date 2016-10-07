b0a1300542f584ba9ce21c8caa1914ce2bd4ab7b
philbort <philbort@users.noreply.github.com>
2016-10-07 15:56:07 -0700

    Add complexity

--- 263_ugly_number.cpp
+++ 263_ugly_number.cpp
@@ -12,20 +12,15 @@ Note that 1 is typically treated as an ugly number.
 */
 
 class Solution {
-
 public:
-
+    // Time: O(logn)
+    // Space: O(1)
     bool isUgly(int num) {
-    	
-        if (num < 1)
-            return false;
-        while (num%2 == 0)
-            num /= 2;
-        while (num%3 == 0)
-            num /= 3;
-        while (num%5 == 0)
-            num /= 5;
-        return ( num == 1);
+        if (num < 1)    return false;
+        while (num % 2 == 0)  num /= 2;
+        while (num % 3 == 0)  num /= 3;
+        while (num % 5 == 0)  num /= 5;
+        return num == 1;
         
     }
 };

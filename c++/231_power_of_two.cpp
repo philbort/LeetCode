123e7d66bcddaf9f36e186482a2467fa28198eaa
philbort <philbort@users.noreply.github.com>
2017-01-07 22:29:23 -0800

    add complexity

--- c++/231_power_of_two.cpp
+++ c++/231_power_of_two.cpp
@@ -8,11 +8,10 @@ Given an integer, write a function to determine if it is a power of two.
 
 
 class Solution {
-
 public:
-
+    // Time: O(1)
+    // Space: O(1)
     bool isPowerOfTwo(int n) {
-
        return n > 0 && !( n&(n-1) );
     }
 };
